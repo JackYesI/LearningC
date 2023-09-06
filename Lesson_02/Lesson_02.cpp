4a85de604cdc7dd5cf3ad844adc7cbca05c1db1c
dev <dev@A26PC15>
2023-09-06 16:12:12 +0300

    Lesson work

--- Lesson_02/Lesson_02.cpp
+++ Lesson_02/Lesson_02.cpp
@@ -8,8 +8,109 @@ using std::cin;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    /*std::cout << "Hello World!\n";
     const double PI = 3.14;
+    int day;
+    cout << "Entrer number day :: ";
+    cin >> day;
+
+    if (day == 1) {
+        cout << "Monday" << endl;
+    }
+    else if (day == 2) {
+        cout << "Tuesday" << endl;
+    }
+    else if (day == 3) {
+        cout << "Wendesday" << endl;
+    }
+    else
+    {
+        cout << "Error" << endl;
+    }*/
+    /*int day;
+    cin >> day;
+    if (0 <= day and day <= 20) {
+        cout << "True";
+    }
+    else {
+        cout << "False";
+    }*/
+    /*int month;
+    cout << "Enter number mounth :: ";
+    cin >> month;
+
+    switch (month)
+    {
+    case 1:
+        cout << "31 days" << endl;
+        break;
+    case 2:
+        int year;
+        cout << "Enter year: ";
+        cin >> year;
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            cout << "29 days" << endl;
+        } 
+        else {
+            cout << "28 days" << endl;
+        }
+        cout << "28 or 29" << endl;
+        break;
+    case 3:
+        cout << "31" << endl;
+        break;
+    case 4:
+        cout << "31" << endl;
+        break;
+    case 5:
+        cout << "31" << endl;
+        break;
+    case 6:
+        cout << "31" << endl;
+        break;
+    case 7: 
+    case 8:
+    case 9: 
+    case 10: 
+    case 11: 
+    case 12:
+        cout << "31" << endl;
+        break;
+    default:
+        cout << "Error" << endl;
+    }*/
+
+    cout << "\t 1 - Red; \n\t 2- Green; \n\t 3 - Blue; \n\t 4 - Yellow; \n\t 5 - Purple; \n\t Enter :: ";
+    int number;
+    bool x = 1;
+    while (x) {
+
+
+        cin >> number;
+
+        switch (number)
+        {
+        case 1:
+            system("Color 40");
+            break;
+        case 2:
+            system("Color 20");
+            break;
+        case 3:
+            system("Color 10");
+            break;
+        case 4:
+            system("Color 60");
+            break;
+        case 5:
+            system("Color 50");
+            break;
+        default:
+            x = 0;
+            break;
+        }
+    }
+
 
 }
 
