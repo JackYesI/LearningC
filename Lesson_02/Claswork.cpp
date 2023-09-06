63d8c31da4b8e1b6b227df41523b353b734bdb95
dev <dev@A26PC15>
2023-09-06 17:34:31 +0300

    Claswork

---
+++ Lesson_02/Claswork.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+enum MyEnum
+{
+	ONE = 1, TWO, THREE, FOUR
+};
+
+int main() {
+	// First
+	cout << "First exersise" << endl;
+	int number;
+	cout << "Enter number :: ";
+	cin >> number;
+	if (number % 2 == 0) {
+		cout << number << " is couple" << endl;
+	}
+	else {
+		cout << number << " is't couple" << endl;
+
+	}
+	cout << endl;
+	// Second
+	cout << "Second exersise" << endl;
+	int a, b;
+	cout << "Enter a :: ";
+	cin >> a;
+	cout << "Enter b :: ";
+	cin >> b;
+	if (a < b) {
+		cout << "a < b";
+	}
+	else if (a > b) {
+		cout << "a > b";
+	}
+	else {
+		cout << " a = b";
+	}
+	cout << endl;
+	cout << endl;
+	// Thirth
+	int num;
+	cout << "Thirth exersise" << endl;
+	cout << "Enterr number :: ";
+	cin >> num;
+	if (num > 0) {
+		cout << "number is positive";
+	}
+	else if (num < 0) {
+		cout << "number is negative";
+	}
+	else {
+		cout << "number equal zero";
+	}
+	cout << endl;
+	cout << endl;
+	// Fourth
+	cout << "Fourth exersise" << endl;
+	int first_num, second_num;
+	cout << "enter first number :: ";
+	cin >> first_num;
+	cout << "Enter second number :: ";
+	cin >> second_num;
+	if (first_num > second_num) {
+		cout << second_num << endl << first_num;
+	}
+	else if (first_num < second_num) {
+		cout << first_num << endl << second_num;
+	}
+	else {
+		cout << "number are equal";
+	}
+	cout << endl;
+	cout << endl;
+	// Second part
+	cout << "First exersise" << endl;
+	int first_mark, second_mark, thirth_mark, fourth_mark, fifth_mark;
+	double average_mark;
+	cout << "Enter five marks : ";
+	cin >> first_mark >> second_mark >> thirth_mark >> fourth_mark >> fifth_mark;
+	average_mark = (first_mark + second_mark + thirth_mark + fourth_mark + fifth_mark) / 5;
+	if (average_mark < 4) {
+		cout << "not allowed for examination";
+	}
+	else {
+		cout << "allowed for examination";
+	}
+	cout << endl;
+	cout << endl;
+	// second exersise
+	cout << "Second exersise" << endl;
+	int number_for_cheking;
+	cout << "Enter number :: ";
+	cin >> number_for_cheking;
+	if (number_for_cheking % 2 == 0) {
+		cout << number_for_cheking * 3 << endl;
+	}
+	else if (number_for_cheking == 0) {
+		cout << "equal zero" << endl;
+	}
+	else {
+		cout << double(double(number) / 2) << endl;
+
+	}
+	cout << endl;
+	// Thirth exersise
+	int x, y, choose;
+	cout << "thirth exersise" << endl;
+	cout << "Enter x :: ";
+	cin >> x;
+	cout << "Enter y :: ";
+	cin >> y;
+	cout << "Choose your variant\n\t1) add\n\t2) diferent\n\t3) multiplication\n\t4) division\n";
+	cin >> choose;
+
+	switch (choose)
+	{
+	case ONE:
+		cout << x + y << endl;
+		break;
+	case TWO:
+		cout << x - y << endl;
+		break;
+	case THREE:
+		cout << x * y << endl;
+		break;
+	case FOUR:
+		cout << double(double(x) / double(y)) << endl;
+		break;
+	default:
+		cout << "Error\n";
+		break;
+	}
+}
--- Lesson_02/Lesson_02.cpp
+++ Lesson_02/Lesson_02.cpp
@@ -1,126 +1,137 @@
-// Lesson_02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//// Lesson_02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+////
 //
-
-#include <iostream>
-using std::cout;
-using std::endl;
-using std::cin;
-
-int main()
-{
-    /*std::cout << "Hello World!\n";
-    const double PI = 3.14;
-    int day;
-    cout << "Entrer number day :: ";
-    cin >> day;
-
-    if (day == 1) {
-        cout << "Monday" << endl;
-    }
-    else if (day == 2) {
-        cout << "Tuesday" << endl;
-    }
-    else if (day == 3) {
-        cout << "Wendesday" << endl;
-    }
-    else
-    {
-        cout << "Error" << endl;
-    }*/
-    /*int day;
-    cin >> day;
-    if (0 <= day and day <= 20) {
-        cout << "True";
-    }
-    else {
-        cout << "False";
-    }*/
-    /*int month;
-    cout << "Enter number mounth :: ";
-    cin >> month;
-
-    switch (month)
-    {
-    case 1:
-        cout << "31 days" << endl;
-        break;
-    case 2:
-        int year;
-        cout << "Enter year: ";
-        cin >> year;
-        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
-            cout << "29 days" << endl;
-        } 
-        else {
-            cout << "28 days" << endl;
-        }
-        cout << "28 or 29" << endl;
-        break;
-    case 3:
-        cout << "31" << endl;
-        break;
-    case 4:
-        cout << "31" << endl;
-        break;
-    case 5:
-        cout << "31" << endl;
-        break;
-    case 6:
-        cout << "31" << endl;
-        break;
-    case 7: 
-    case 8:
-    case 9: 
-    case 10: 
-    case 11: 
-    case 12:
-        cout << "31" << endl;
-        break;
-    default:
-        cout << "Error" << endl;
-    }*/
-
-    cout << "\t 1 - Red; \n\t 2- Green; \n\t 3 - Blue; \n\t 4 - Yellow; \n\t 5 - Purple; \n\t Enter :: ";
-    int number;
-    bool x = 1;
-    while (x) {
-
-
-        cin >> number;
-
-        switch (number)
-        {
-        case 1:
-            system("Color 40");
-            break;
-        case 2:
-            system("Color 20");
-            break;
-        case 3:
-            system("Color 10");
-            break;
-        case 4:
-            system("Color 60");
-            break;
-        case 5:
-            system("Color 50");
-            break;
-        default:
-            x = 0;
-            break;
-        }
-    }
-
-
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//#include <iostream>
+//using std::cout;
+//using std::endl;
+//using std::cin;
+//
+//enum MyEnum
+//{
+//    red = 1, green, blue, yellow, purple
+//};
+//
+//int main()
+//{
+//    /*std::cout << "Hello World!\n";
+//    const double PI = 3.14;
+//    int day;
+//    cout << "Entrer number day :: ";
+//    cin >> day;
+//
+//    if (day == 1) {
+//        cout << "Monday" << endl;
+//    }
+//    else if (day == 2) {
+//        cout << "Tuesday" << endl;
+//    }
+//    else if (day == 3) {
+//        cout << "Wendesday" << endl;
+//    }
+//    else
+//    {
+//        cout << "Error" << endl;
+//    }*/
+//    /*int day;
+//    cin >> day;
+//    if (0 <= day and day <= 20) {
+//        cout << "True";
+//    }
+//    else {
+//        cout << "False";
+//    }*/
+//    /*int month;
+//    cout << "Enter number mounth :: ";
+//    cin >> month;
+//
+//    switch (month)
+//    {
+//    case 1:
+//        cout << "31 days" << endl;
+//        break;
+//    case 2:
+//        int year;
+//        cout << "Enter year: ";
+//        cin >> year;
+//        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+//            cout << "29 days" << endl;
+//        } 
+//        else {
+//            cout << "28 days" << endl;
+//        }
+//        cout << "28 or 29" << endl;
+//        break;
+//    case 3:
+//        cout << "31" << endl;
+//        break;
+//    case 4:
+//        cout << "31" << endl;
+//        break;
+//    case 5:
+//        cout << "31" << endl;
+//        break;
+//    case 6:
+//        cout << "31" << endl;
+//        break;
+//    case 7: 
+//    case 8:
+//    case 9: 
+//    case 10: 
+//    case 11: 
+//    case 12:
+//        cout << "31" << endl;
+//        break;
+//    default:
+//        cout << "Error" << endl;
+//    }*/
+//
+//    /*cout << "\t 1 - Red; \n\t 2- Green; \n\t 3 - Blue; \n\t 4 - Yellow; \n\t 5 - Purple; \n\t Enter :: ";
+//    int number;
+//    bool x = 1;
+//    while (x) {
+//
+//
+//        cin >> number;
+//
+//        switch (number)
+//        {
+//        case red:
+//            system("Color 40");
+//            break;
+//        case green:
+//            system("Color 20");
+//            break;
+//        case blue:
+//            system("Color 10");
+//            break;
+//        case yellow:
+//            system("Color 60");
+//            break;
+//        case purple:
+//            system("Color 50");
+//            break;
+//        default:
+//            x = 0;
+//            break;
+//        }
+//    }*/
+//    /*int day;
+//    int year = 2005;
+//    day (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) ? 29 : 28;
+//    cout << day << endl;
+//    int h = 9, m = 5, s = 25;
+//    cout << (h < 10 ? "0" : "") << h << ":" << (m < 10 ? "0" : "") << (s < 10 ? "": " ") << s << endl;*/
+//
+//
+//}
+//
+//// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+//// Debug program: F5 or Debug > Start Debugging menu
+//
+//// Tips for Getting Started: 
+////   1. Use the Solution Explorer window to add/manage files
+////   2. Use the Team Explorer window to connect to source control
+////   3. Use the Output window to see build output and other messages
+////   4. Use the Error List window to view errors
+////   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+////   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
